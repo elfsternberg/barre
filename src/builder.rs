0e93a3f27bfd0fcb80d4c30d279a8dd563d2d313
Elf M. Sternberg <elf.sternberg@gmail.com>
2018-11-14 11:40:07 -0800

    FEAT Fixed point analysis of nodes.
    Implement the fixed point analysis of nodes using the code flow technique
    from Adams (2011).  This is a best-guess effort at implementing the code
    in the Scheme version, minus the Reduction node feature, which
    I still don't have an easy answer for in Rust.  The tests pass, so the
    "nullability?" test works on the non-recursive version.  Anything more
    than that is... beyond me at this point.

--- src/builder.rs
+++ src/builder.rs
@@ -1,39 +1,42 @@
 use language::Language;
-use types::{Node, NodeId, Siaa};
+use types::{Alt, Cat, Emp, Eps, Node, NodeId, Rep, Siaa, Tok};
 
 pub fn init_barre_vec<T: Siaa>() -> Vec<Node<T>> {
-    vec![Node::Eps(T::default()), Node::Emp]
+    vec![Node::Emp(Emp::new())]
 }
 
-pub fn language_to_vec_rep<T: Siaa>(lang: &Language<T>) -> Vec<Node<T>> {
+pub fn language_to_vec_rep<T: Siaa + 'static>(lang: &Language<T>) -> Vec<Node<T>> {
     let mut new_representation = init_barre_vec();
 
-    fn language_handler<T: Siaa>(lang: &Language<T>, r: &mut Vec<Node<T>>) -> NodeId {
+    fn language_handler<T: Siaa + 'static>(lang: &Language<T>, r: &mut Vec<Node<T>>) -> NodeId {
         match lang {
-            Language::Epsilon => 0,
+            Language::Epsilon => {
+                r.push(Node::Eps(Eps::new(T::default())));
+                r.len() - 1
+            }
 
             Language::Token(ref t) => {
-                r.push(Node::Tok(t.0.clone()));
+                r.push(Node::Tok(Tok::new(t.0.clone())));
                 r.len() - 1
             }
 
             Language::Alt(ref node) => {
                 let car = language_handler(&node.0, r);
                 let cdr = language_handler(&node.1, r);
-                r.push(Node::Alt(car, cdr));
+                r.push(Node::Alt(Alt::new(car, cdr)));
                 r.len() - 1
             }
 
             Language::Cat(ref node) => {
                 let car = language_handler(&node.0, r);
                 let cdr = language_handler(&node.1, r);
-                r.push(Node::Cat(car, cdr));
+                r.push(Node::Cat(Cat::new(car, cdr)));
                 r.len() - 1
             }
 
             Language::Repeat(ref node) => {
                 let car = language_handler(&node.0, r);
-                r.push(Node::Rep(car));
+                r.push(Node::Rep(Rep::new(car)));
                 r.len() - 1
             }
         }
